ad715eb184185ac48ff8fb48134ec4a0f2bcb369
Piggest <piggest@mail.ru>
2018-01-12 05:37:10 +0300

    prototype Cell

--- coder-swag/coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CategoriesVC.swift
 //  coder-swag
 //
 //  Created by Admin on 12.01.18.
@@ -8,17 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
